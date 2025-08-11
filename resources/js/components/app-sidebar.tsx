76c600d9c4d1414fcb398a7e1a2a5ff1dad3a8ff
Pridoh <freedom.b4li@gmail.com>
2025-08-11 15:02:55 +0700

    feat: set repository link on sidebar

--- resources/js/components/app-sidebar.tsx
+++ resources/js/components/app-sidebar.tsx
@@ -18,13 +18,8 @@ const mainNavItems: NavItem[] = [
 const footerNavItems: NavItem[] = [
     {
         title: 'Repository',
-        href: 'https://github.com/laravel/react-starter-kit',
+        href: 'https://github.com/Pridoh/Laravel-Mini-Project.git',
         icon: Folder,
-    },
-    {
-        title: 'Documentation',
-        href: 'https://laravel.com/docs/starter-kits#react',
-        icon: BookOpen,
     },
 ];
 
